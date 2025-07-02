d3ba0fce51495d25e679f4a426385af180589405
MeenaPrajapat <meenaprajapat98132@gmail.com>
2025-07-02 05:21:57 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+
+const accountId = 144553
+let accountEmail = "hitesh@google"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2
+/*
+Prefer not to use var
+beacause of issue in block scope and functional scope
+*/
+console.log(accountId);
+console.table([accountId,accountEmail, accountPassword, accountCity])
