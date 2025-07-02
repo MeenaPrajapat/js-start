558f7846cee5e7a619d8ede4410036e74b8cc494
MeenaPrajapat <meenaprajapat98132@gmail.com>
2025-07-02 05:12:40 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+
+"use strict"; // treat all JS code as newer version 
+
+// alert( 3 + 3) // we are using nodejs not browser
+// code readability should be high
+
+// number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+//object
+
+console.log(typeof "hitesh")
+console.log(typeof 23)
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof true)
