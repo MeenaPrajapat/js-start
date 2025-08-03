8bbcd8085185c2451703587f9aeba702f8ebf9e2
MeenaPrajapat <meenaprajapat98132@gmail.com>
2025-08-03 13:57:21 +0000

    for each , filter

---
+++ 05_iterations/six.js
@@ -0,0 +1,47 @@
+const coding = ["js", "ruby", "python", "java", "cpp"]
+
+// ****note***** :for each koi bhi value return nhi krta h
+// const values = coding.forEach((item) => {
+//     // console.log(item);
+//     return item
+// })
+
+// console.log(values);
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+// const newNums = myNums.filter((num) => num > 4)
+
+// const newNums = myNums.filter((num) =>{ //{} scope open krte h toh return likhna pdega
+//     return num > 4
+// })
+
+// console.log(newNums);
+
+const newNums = []
+
+myNums.forEach((num)=> {
+    if(num>4){
+        newNums.push(num)
+    }
+})
+
+console.log(newNums);
+
+const books = [
+  { title: 'Book One' , genre: 'Fiction' , publish: 1981, edition: 2004 },
+  { title: 'Book Two' , genre: 'Non-Fiction' , publish: 1992, edition: 2008 },
+  { title: 'Book Three' , genre: 'History' , publish: 1999, edition: 2007 },
+  { title: 'Book Four' , genre: 'Non-Fiction' , publish: 1994, edition: 2010 },
+  { title: 'Book Five' , genre: 'Science' , publish: 2009, edition: 2014 },
+  { title: 'Book Six' , genre: 'Fiction' , publish: 1987, edition: 2010 },
+  { title: 'Book Seven' , genre: 'History' , publish: 1986, edition: 1996 },
+  { title: 'Book Eight' , genre: 'Science' , publish: 2011, edition: 2016 },
+  { title: 'Book Nine' , genre: 'Non-Fiction' , publish: 1981, edition: 1989 },
+];
+
+let userBooks = books.filter((bk)=> bk.genre == 'History')
+userBooks = books.filter((bk)=> bk.publish >= 2000)
+userBooks = books.filter((bk)=> {
+  return bk.publish >= 1995 && bk.genre === "History"
+})
+console.log(userBooks);
